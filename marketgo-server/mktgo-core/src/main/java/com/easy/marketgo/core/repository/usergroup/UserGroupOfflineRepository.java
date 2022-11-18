f8421bc4fff542b3fe606e52d060b2eeb2c0069f
wang_kevin <287541200@qq.com>
2022-11-18 22:41:44 +0800

    send offline user group

--- marketgo-server/mktgo-core/src/main/java/com/easy/marketgo/core/repository/usergroup/UserGroupOfflineRepository.java
+++ marketgo-server/mktgo-core/src/main/java/com/easy/marketgo/core/repository/usergroup/UserGroupOfflineRepository.java
@@ -1,10 +1,10 @@
 package com.easy.marketgo.core.repository.usergroup;
 
 import com.easy.marketgo.core.entity.usergroup.UserGroupOfflineEntity;
-import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * @author : kevinwang
@@ -13,14 +13,8 @@ import org.springframework.data.repository.query.Param;
  * Describe:
  */
 public interface UserGroupOfflineRepository extends CrudRepository<UserGroupOfflineEntity, Long> {
-    @Modifying
-    @Query("UPDATE user_group__offline SET estimate_result = :estimate_result, status = " +
-            ":status WHERE request_id = :request_id AND project_uuid = :project_uuid")
-    int updateResultByRequestId(@Param("request_id") String requestId,
-                                @Param("project_uuid") String projectId,
-                                @Param("estimate_result") String estimateResult,
-                                @Param("status") String status);
-
+    @Query("SELECT DISTINCT(external_user_id) FROM user_group_offline WHERE corp_id = :corpId AND uuid = :uuid")
+    List<String> queryMemberByUuid(String corpId, String uuid);
 
     @Query("SELECT COUNT(DISTINCT(external_user_id)) FROM user_group_offline WHERE corp_id = :corpId AND uuid = :uuid")
     Integer queryExternalUserCountByUuid(String corpId, String uuid);
@@ -28,8 +22,7 @@ public interface UserGroupOfflineRepository extends CrudRepository<UserGroupOffl
     @Query("SELECT COUNT(DISTINCT(member_id)) FROM user_group_offline WHERE corp_id = :corpId AND uuid = :uuid")
     Integer queryMemberCountByUuid(String corpId, String uuid);
 
-    @Modifying
-    @Query("UPDATE user_group__offline SET status = :status WHERE uuid = :uuid AND project_uuid = :project_uuid")
-    int updateStatusByUuid(@Param("project_uuid") String projectId, @Param("uuid") String uuid,
-                           @Param("status") String status);
+    @Query("SELECT DISTINCT(external_user_id) FROM user_group_offline WHERE corp_id = :corpId AND uuid = :uuid AND " +
+            "member_id=:memberId")
+    List<String> queryExternalUsersByUuidAndMemberId(String corpId, String uuid, String memberId);
 }
