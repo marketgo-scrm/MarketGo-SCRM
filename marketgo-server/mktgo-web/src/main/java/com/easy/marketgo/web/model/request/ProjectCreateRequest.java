d1de7f23c1d8ab9b154a78e730985c87a13ed2bb
wang_kevin <287541200@qq.com>
2023-02-15 11:11:07 +0800

    add create project interface

---
+++ marketgo-server/mktgo-web/src/main/java/com/easy/marketgo/web/model/request/ProjectCreateRequest.java
@@ -0,0 +1,24 @@
+package com.easy.marketgo.web.model.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author : kevinwang
+ * @version : 1.0
+ * @data : 6/22/22 3:38 PM
+ * Describe:
+ */
+@Data
+@ApiModel(description = "项目创建request")
+public class ProjectCreateRequest {
+    @ApiModelProperty(value = "任务id")
+    private Integer id;
+    @ApiModelProperty(value = "任务uuid")
+    private String uuid;
+    @ApiModelProperty(value = "项目名称", required = true)
+    private String name;
+    @ApiModelProperty(value = "项目描述", required = true)
+    private String desc;
+}
