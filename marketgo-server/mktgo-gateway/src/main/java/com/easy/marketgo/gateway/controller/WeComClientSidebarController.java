e70f3655896010ffd7e4e3cd8c8546a922c36d94
wang_kevin <287541200@qq.com>
2023-02-24 12:42:54 +0800

    change taskuuid param

--- marketgo-server/mktgo-gateway/src/main/java/com/easy/marketgo/gateway/controller/WeComClientSidebarController.java
+++ marketgo-server/mktgo-gateway/src/main/java/com/easy/marketgo/gateway/controller/WeComClientSidebarController.java
@@ -43,7 +43,7 @@ public class WeComClientSidebarController {
                     "TASK_CENTER_SEND_CONTENT") @Valid @RequestParam(value =
                     "content_type", required = true, defaultValue = "TASK_CENTER_SEND_CONTENT") String contentType,
             @ApiParam(value = "员工ID", required = false) @Valid @RequestParam(value = "member_id", required = false) String memberId,
-            @ApiParam(value = "任务ID", required = false) @RequestParam("task_uuid") String taskUuid) {
+            @ApiParam(value = "任务ID", required = false) @RequestParam(value = "task_uuid", required = false) String taskUuid) {
         return ResponseEntity.ok(sidebarManagerService.getSidebarContent(corpId, contentType, memberId, taskUuid));
     }
 }
