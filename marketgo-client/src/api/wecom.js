ba5371f1e15775a651a2060cd1969c4ec589513c
chenyx755 <chenyx755@chinaunicom.cn>
2023-06-08 18:03:17 +0800

    feat: jssdk

--- marketgo-client/src/api/wecom.js
+++ marketgo-client/src/api/wecom.js
@@ -38,31 +38,6 @@ export const wecom = {
       `/mktgo/client/wecom/task_center/sub_list?${qs.stringify(query)}`
     );
   },
-  chatScriptList: (query = {}) => {
-    return xhr.get(`/mktgo/client/chatscript/list?${qs.stringify(query)}`);
-  },
-  chatScriptDetail: (query = {}) => {
-    return xhr.get(`/mktgo/client/chatscript/detail?${qs.stringify(query)}`);
-  },
-  chatScriptCreators: (query = {}) => {
-    handleQuery(query)
-    return xhr.get(`/mktgo/client/chatscript/creators?${qs.stringify(query)}`);
-  },
-  questionList: (query = {}) => {
-    handleQuery(query)
-    return xhr.get(`/mktgo/client/question/list?${qs.stringify(query)}`);
-  },
-  materialList: (query = {}) => {
-    handleQuery(query)
-    return xhr.get(`/mktgo/client/material/list?${qs.stringify(query)}`);
-  },
-  materialDetail: (query = {}) => {
-    handleQuery(query)
-    return xhr.get(`/mktgo/client/material/detail?${qs.stringify(query)}`);
-  },
-  tagList: (query = {}) => {
-    return xhr.get(`/mktgo/client/tag/list?${qs.stringify(query)}`);
-  },
   verifyCode: ({ query = {}, params = {} }) => {
     return xhr.post(
       `/mktgo/client/wecom/verify?${qs.stringify(query)}`,
